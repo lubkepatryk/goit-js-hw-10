f1895896063be2787a6714b0cd14ff48ffeb2ae4
lubkepatryk <lubke.patryk@gmail.com>
2023-03-19 19:17:17 +0100

    updated fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,15 @@
-export const fetchCountries = (name) {
-    const url = `https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages`;
+export const fetchCountries = name => {
+  const url = `https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages`;
 
-    return fetch(url)
-    .then((response => {
+  return fetch(url)
+    .then(response => {
       if (!response.ok) {
         throw new Error(response.statusText);
       }
       return response.json();
     })
-    .then((data) => data)
-    .catch((error) => {
-        throw new Error(error.message);
+    .then(data => data)
+    .catch(error => {
+      throw new Error(error.message);
     });
-  };
+};
